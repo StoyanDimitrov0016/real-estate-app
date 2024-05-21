c719646e0342d5f47d5883becfa4a8dccaaef0ae
Stoyan Dimitrov <stoyan1020@gmail.com>
2024-05-21 22:53:06 +0300

    feat(frontend): Implement Layout component to the app and append Home, List, and Single pages to corresponding paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
-import Navbar from "./components/navbar/Navbar";
-import "./layout.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./routes/home-page/HomePage";
+import ListPage from "./routes/list-page/ListPage";
+import Layout from "./layout/Layout";
+import SinglePage from "./routes/single-page/SinglePage";
 
 function App() {
-  return (
-    <div className="layout">
-      <Navbar />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "/", element: <HomePage /> },
+        { path: "/list", element: <ListPage /> },
+        { path: "/:id", element: <SinglePage /> },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
